6dfb7f693258ca8225745e3cfbc5e9a5da25d122
mikaahopelto <69237295+mikaahopelto@users.noreply.github.com>
2022-05-20 22:27:06 +0300

    Added test message sending to rabbitmq

--- sofar/main.go
+++ sofar/main.go
@@ -21,7 +21,35 @@ func main() {
 		ch, err := conn.Channel()
 		if err != nil {
 			fmt.Printf("Failed to open channel. %s", err)
+			continue
 		}
 		defer ch.Close()
+		// We create a Queue to send the message to.
+		q, err := ch.QueueDeclare(
+			"sofar-queue", // name
+			false,         // durable
+			false,         // delete when unused
+			false,         // exclusive
+			false,         // no-wait
+			nil,           // arguments
+		)
+		if err != nil {
+			fmt.Printf("Failed to declare queue. %s", err)
+			continue
+		}
+		body := fmt.Sprintf(`{"Testing": "%s"}`, time.Now().UTC().String())
+		err = ch.Publish(
+			"",     // exchange
+			q.Name, // routing key
+			false,  // mandatory
+			false,  // immediate
+			amqp.Publishing{
+				ContentType: "application/json",
+				Body:        []byte(body),
+			})
+		if err != nil {
+			fmt.Printf("Failed to send message. %s", err)
+			continue
+		}
 	}
 }
